2f3688c14d618c655f1f2add2ee51c35ae042a3a
JoggerChe <120989445+BorovChe@users.noreply.github.com>
2023-05-17 20:38:26 +0300

    push

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
 
+const player = new Player('vimeo-player');
+var throttle = require('lodash.throttle');
+
+player.on('timeupdate', throttle(onTimeVideo, 1000));
+
+function onTimeVideo(even) {
+  const second = even.seconds;
+
+  const currentTime = JSON.stringify(
+    localStorage.setItem('videoplayer-current-time', second)
+  );
+  return currentTime;
+}
+
+const backTime = JSON.parse(
+  localStorage.getItem('videoplayer-current-time', onTimeVideo)
+);
+
+player.setCurrentTime(backTime);
