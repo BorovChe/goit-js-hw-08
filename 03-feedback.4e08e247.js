eb6f8e44b1d4347f5bd2889cfb7799d167cdccdc
BorovChe <120989445+BorovChe@users.noreply.github.com>
2023-05-18 18:44:11 +0000

    Deploying to gh-pages from @ BorovChe/goit-js-hw-08@3f0dc817f2c1463dcd44b7286965ecdfc6f2cc39 🚀

---
+++ 03-feedback.4e08e247.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".feedback-form");e.addEventListener("input",(function(e){const t=e.currentTarget.email.value,l=e.currentTarget.message.value;a={email:`${t}`,message:`${l}`},localStorage.setItem("feedback-form-state",JSON.stringify(a))})),e.addEventListener("submit",(function(t){t.preventDefault(),console.log(a),localStorage.clear(),e.email.value="",e.message.value=""}));let a={};try{const a=JSON.parse(localStorage.getItem("feedback-form-state"));e.email.value=a.email,e.message.value=a.message}catch(e){}
+//# sourceMappingURL=03-feedback.4e08e247.js.map
