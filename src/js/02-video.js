e77b2f0ca06c706c4307189bb4c2dc3670de4ac0
JoggerChe <120989445+BorovChe@users.noreply.github.com>
2023-05-18 23:20:30 +0300

    fix2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -20,4 +20,16 @@ const backTime = JSON.parse(
 
 
 
-player.setCurrentTime(backTime);
+player.setCurrentTime(backTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,32 +1,39 @@
+var throttle = require('lodash.throttle');
 
 
 const formEl = document.querySelector('.feedback-form');
-formEl.addEventListener('input', onInputValue);
-formEl.addEventListener('submit', makeSubmit)
+
+
+formEl.addEventListener('input', throttle(onInputValue, 500));
+formEl.addEventListener('submit', makeSubmit);
 
 let objForm = {};
 
-function onInputValue(e) {
-  const emailValue = e.currentTarget.email.value;
-  const messageValue = e.currentTarget.message.value;
+function onInputValue() {
+  const emailValue = formEl.email.value;
+  const messageValue = formEl.message.value;
 
   objForm = { email: `${emailValue}`, message: `${messageValue}` };
 
-  localStorage.setItem('feedback-form-state', JSON.stringify(objForm)); 
-};
- 
+  localStorage.setItem('feedback-form-state', JSON.stringify(objForm));
+}
+
 try {
   const parseObj = JSON.parse(localStorage.getItem('feedback-form-state'));
   formEl.email.value = parseObj.email;
-formEl.message.value = parseObj.message;
-} catch (error) {
-}
-
+  formEl.message.value = parseObj.message;
+} catch (error) {}
 
 function makeSubmit(e) {
   e.preventDefault();
-  console.log(objForm);
+  if (formEl.email.value === '' || formEl.message.value === '') {
+    alert("Заполните полe!!!");
+  }
+  else {
+    console.log(objForm);
   localStorage.clear();
-formEl.email.value = '';
-formEl.message.value = '';
+  formEl.email.value = '';
+  formEl.message.value = '';
+  };
 };
+
